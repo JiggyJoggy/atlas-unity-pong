75167fa7d6802a511170f5108ae04f07f4ff7733
Jagger Van Winkle <JaggerVanW@gmail.com>
2024-03-11 19:57:11 -0500

    saving progress on task 5

--- pong-one/Assets/Scripts/Ball.cs
+++ pong-one/Assets/Scripts/Ball.cs
@@ -1,40 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float maxInitAngle = 0.67f;
     public float speed;
 
     // Starts and faces towards a random direction
     void Start()
     {
+        speed += speed * 10;
         float num = Random.Range(1, 10);
 
         if (num >= 5)
         {
-            rb.AddForce(Vector2.right * speed);
-            rb.AddForce(Vector2.up * speed);
+            Vector2 dir = Vector2.left * speed;
+            dir.y = Random.Range(-maxInitAngle, maxInitAngle) * 100;
+            rb.velocity = dir;
         }
         else
         {
-            rb.AddForce(Vector2.left * speed);
-            rb.AddForce(Vector2.up * speed);
+            Vector2 dir = Vector2.right * speed;
+            dir.y = Random.Range(-maxInitAngle, maxInitAngle) * 100;
+            rb.velocity = dir;
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Paddle"))
         {
-            Debug.Log("Ball hit paddle"); // Will replace with AddForce
+            if (rb.position.y > other.transform.position.y)
+            {
+                rb.AddForce(Vector2.up * (speed - 600));
+            }
+            if (rb.position.y < other.transform.position.y)
+            {
+                rb.AddForce(Vector2.down * (speed - 600));
+            }
+            rb.velocity *= -1;
         }
     }
 
     // Restarts ball to starting pos
-    public void Restart()
+    public void ResetBall()
     {
         rb.position = new Vector3(960, 540, 0);
+        rb.velocity = Vector3.zero;
     }
 }
--- pong-one/Assets/Scripts/Player.cs
+++ pong-one/Assets/Scripts/Player.cs
@@ -6,15 +6,30 @@ public class Player : MonoBehaviour
 {
     public KeyCode upKey;
     public KeyCode downKey;
-    // Start is called before the first frame update
+    public float speed;
+
     void Start()
     {
-        
+        speed += speed * 100;
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(upKey))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+            if (transform.localPosition.y >= 460)
+            {
+                transform.localPosition = new Vector2(transform.localPosition.x, 455);
+            }
+        }
+        if (Input.GetKey(downKey))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+            if (transform.localPosition.y <= -460)
+            {
+                transform.localPosition = new Vector2(transform.localPosition.x, -455);
+            }
+        }
     }
 }
