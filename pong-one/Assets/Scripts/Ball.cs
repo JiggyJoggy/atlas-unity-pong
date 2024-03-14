92f81a572ff3b6b708c96557d450c5751500ab2e
Jagger Van Winkle <JaggerVanW@gmail.com>
2024-03-13 21:37:58 -0500

    task 8

--- pong-one/Assets/Scripts/Ball.cs
+++ pong-one/Assets/Scripts/Ball.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
@@ -9,24 +12,15 @@ public class Ball : MonoBehaviour
     public float maxInitAngle = 0.67f;
     public float speed;
 
+    float scorePointLeft = 0;
+    float scorePointRight = 0;
+
     // Starts and faces towards a random direction
     void Start()
     {
         speed += speed * 10;
-        float num = Random.Range(1, 10);
-
-        if (num >= 5)
-        {
-            Vector2 dir = Vector2.left * speed;
-            dir.y = Random.Range(-maxInitAngle, maxInitAngle) * 100;
-            rb.velocity = dir;
-        }
-        else
-        {
-            Vector2 dir = Vector2.right * speed;
-            dir.y = Random.Range(-maxInitAngle, maxInitAngle) * 100;
-            rb.velocity = dir;
-        }
+        
+        RandomizeBallMovementStartUp();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -44,6 +38,48 @@ public class Ball : MonoBehaviour
             rb.velocity *= -1;
             rb.rotation *= -1;
         }
+
+        if (other.CompareTag("Goal"))
+        {
+            GameObject LeftGoal = GameObject.Find("LeftGoal");
+            GameObject RightGoal = GameObject.Find("RightGoal");
+            if (other.name == "LeftGoal")
+            {
+                scorePointRight++;
+                GameObject RightScoreText = GameObject.Find("RightScore");
+                RightScoreText.GetComponent<TextMeshProUGUI>().text = scorePointRight.ToString();
+                if (scorePointRight == 11)
+                {
+                    RightGoal.GetComponent<Image>().color = Color.yellow;
+                    RightGoal.GetComponentInChildren<TextMeshProUGUI>().text = "Winner!";
+                    LeftGoal.GetComponent<Image>().color = Color.red;
+                    LeftGoal.GetComponentInChildren<TextMeshProUGUI>().text = "Loser...";
+                    GameOver();
+                }
+                else
+                {
+                    ResetBall();
+                }
+            }
+            if (other.name == "RightGoal")
+            {
+                scorePointLeft++;
+                GameObject LeftScoreText = GameObject.Find("LeftScore");
+                LeftScoreText.GetComponent<TextMeshProUGUI>().text = scorePointLeft.ToString();
+                if (scorePointLeft == 11)
+                {
+                    LeftGoal.GetComponent<Image>().color = Color.yellow;
+                    LeftGoal.GetComponentInChildren<TextMeshProUGUI>().text = "Winner!";
+                    RightGoal.GetComponent<Image>().color = Color.red;
+                    RightGoal.GetComponentInChildren<TextMeshProUGUI>().text = "Loser...";
+                    GameOver();
+                }
+                else
+                {
+                    ResetBall();
+                }
+            }
+        }
     }
 
     // Restarts ball to starting pos
@@ -51,5 +87,29 @@ public class Ball : MonoBehaviour
     {
         rb.position = new Vector3(960, 540, 0);
         rb.velocity = Vector3.zero;
+        RandomizeBallMovementStartUp();
+    }
+
+    public void RandomizeBallMovementStartUp()
+    {
+        float num = Random.Range(1, 10);
+
+        if (num >= 5)
+        {
+            Vector2 dir = Vector2.left * speed;
+            dir.y = Random.Range(-maxInitAngle, maxInitAngle) * 100;
+            rb.velocity = dir;
+        }
+        else
+        {
+            Vector2 dir = Vector2.right * speed;
+            dir.y = Random.Range(-maxInitAngle, maxInitAngle) * 100;
+            rb.velocity = dir;
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("We have a winner!");
     }
 }
